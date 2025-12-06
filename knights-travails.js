34bbcd11bc2b92eafa44bc9903549f0936b6d9fc
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-06 12:48:24 +0200

    Add pseudocode

---
+++ knights-travails.js
@@ -0,0 +1,8 @@
+// Pseudocode
+
+// Show the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
+// [x, y] are coordinates between 0, 7.
+// Use a queue.
+// Track visited positions.
+// From every square, multiple moves are possible.
+// Use search algorithm to find the shortest path between the starting square (or node) and the ending square.
